07725c8424aa5ee4b8bcb2d49ea26fb671f76819
Loïc <loic.kervellec@polytechnique.edu>
2022-01-14 13:57:46 +0100

    adding tests

--- tests/test_data_train.py
+++ tests/test_data_train.py
@@ -2,7 +2,7 @@ import torch
 
 data = torch.load("data/processed/train.pt") #datas
 
-assert len(set([label for _, label in data])) == 3, "Not the good number of classes"
+assert len(set([label for _, label in data])) == 29, "Not the good number of classes"
 
 for i in range(0,len(data)): #good size
     assert data[i][0].shape == torch.Size([3, 224, 224]), "No the good image size"
