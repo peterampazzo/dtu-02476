fdf2846724f349b2c093a2e0fa14c53dd148eb42
Loïc <loic.kervellec@polytechnique.edu>
2022-01-14 17:51:34 +0100

    test for datas and model (modified)

--- tests/test_data_test.py
+++ tests/test_data_test.py
@@ -1,7 +1,18 @@
 import torch
+import random
+import os
+import pytest
 
-data = torch.load("data/processed/test.pt") #datas
 
-assert len(set([label for _, label in data])) == 29, "Not the good number of classes"
-for i in range(0,len(data)): #good size
-    assert data[i][0].shape == torch.Size([3, 224, 224]), "No the good image size"
+
+@pytest.mark.skipif(not os.path.exists('data/processed/test.pt'), reason="No Datas")
+
+def test_length():
+    data = torch.load("data/processed/test.pt") #datas
+    assert len(set([label for _, label in data])) == 3, "Not the good number of classes"
+
+def test_shape():
+    data = torch.load("data/processed/test.pt") #datas
+    randomlist = random.sample(range(0, len(data)-1), 200)
+    for i in randomlist: #good size
+        assert data[i][0].shape == torch.Size([3, 224, 224]), "No the good image size"
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,13 @@
 import torch
 from src.models.conv_nn import ConvNet
+import random
+import os
+import pytest
 
-data = torch.load("data/processed/train.pt") 
-data = torch.utils.data.DataLoader(data, batch_size=64, shuffle=True)#datas
-model = ConvNet(1028,512)
+@pytest.mark.skipif(not os.path.exists('data/processed/test.pt'), reason="No Datas")
 
-assert model(iter(data).next()[0]).shape == torch.Size([64, 29]), "Not the good output"
+def test_model():
+    data = torch.load("data/processed/train.pt") 
+    data = torch.utils.data.DataLoader(data, batch_size=64, shuffle=True)#datas
+    model = ConvNet(1028,512)
+    assert model(iter(data).next()[0]).shape == torch.Size([64, 29]), "Not the good output"
