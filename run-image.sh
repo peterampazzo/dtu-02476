d996deb5982125951b106a58f7731a147b02bd44
Pietro Rampazzo <pietro@rampazzo.co>
2022-01-15 10:11:48 +0100

    make run-image.sh more generic

--- run-image.sh
+++ run-image.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    tag="latest"
+  else
+    tag=$2
+fi
+
+image="${1}:${tag}"
+echo "Running: $image";
+
 docker run -d \
   --mount type=bind,src="$(pwd)"/data,dst=/app/data \
   --mount type=bind,src="$(pwd)"/models,dst=/app/models \
-  -d train:latest
+  -d $image
