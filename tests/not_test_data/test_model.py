bb191ef01057435750d1a0846cc8e81b66dd2cda
Pietro Rampazzo <pietro@rampazzo.co>
2022-01-17 17:11:10 +0100

    run tox and fix flake8 error

--- tests/not_test_data/test_model.py
+++ tests/not_test_data/test_model.py
@@ -1,17 +1,18 @@
 import torch
+
 from src.models.conv_nn import ConvNet
 
 
 def test_model_output():
-    data = torch.rand([64, 3, 224, 224]) # random datas
-    model = ConvNet(1028,512)
+    data = torch.rand([64, 3, 224, 224])  # random datas
+    model = ConvNet(1028, 512)
     assert model(data).shape == torch.Size([64, 29]), "Not the good output"
 
-def test_model_layers():
-    model = ConvNet(1028,512)
-    all_layer=[]
-    for i in model.modules():
-        all_layer+=[i]
-    nb_layer=len(all_layer)-2
-    assert nb_layer==13, "Not the good number of layer"
 
+def test_model_layers():
+    model = ConvNet(1028, 512)
+    all_layer = []
+    for i in model.modules():
+        all_layer += [i]
+    nb_layer = len(all_layer) - 2
+    assert nb_layer == 13, "Not the good number of layer"
