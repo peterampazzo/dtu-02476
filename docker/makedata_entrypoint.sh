fac1c455d0735cfe57182701ea7683a551fe1c50
Alessandro212977 <s212977@dtu.dk>
2022-01-18 14:09:17 +0100

    create key before authentication

--- docker/makedata_entrypoint.sh
+++ docker/makedata_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
-gcloud auth activate-service-account 562525590119-compute@developer.gserviceaccount.com --key-file=/path/key.json --project=dtumlops-338418
+gcloud iam service-account keys create my_key.json --iam-account=562525590119-compute@developer.gserviceaccount.com
+gcloud auth activate-service-account 562525590119-compute@developer.gserviceaccount.com --key-file=my_key.json --project=dtumlops-338418
 gcsfuse dtumlopsdata /data #or /root/data???
 python -u src/data/make_dataset.py
