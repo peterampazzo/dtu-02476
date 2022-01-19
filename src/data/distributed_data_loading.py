1b9b6b8dd9ea0682fe022b22cc0b641d29688e63
Loïc <loic.kervellec@polytechnique.edu>
2022-01-19 16:22:49 +0100

    update distribution

--- src/data/distributed_data_loading.py
+++ src/data/distributed_data_loading.py
@@ -5,19 +5,20 @@ import time
 import numpy as np
 import torch
 
-nb_core = multiprocessing.cpu_count()
+train = torch.load("data/processed/train.pt")
 
-core = [i for i in range(0, nb_core + 1)]
-deviations = []
-timings = []
-
-if __name__ == "__main__":
+def time_per_core():
+    nb_core = multiprocessing.cpu_count()
+    core = [i for i in range(0, nb_core + 1)]
+    print(nb_core)
+    deviations = []
+    timings = []
     for i in core:
-        res = []
+        res = list()
+        print(i)
         for j in range(0, 5):
             start = time.time()
 
-            train = torch.load("data/processed/train.pt")
             train_set = torch.utils.data.DataLoader(
                 train, batch_size=90, shuffle=True, num_workers=i
             )
@@ -28,9 +29,9 @@ if __name__ == "__main__":
 
             end = time.time()
 
-            res += [end - start]
+            res.append(end - start)
 
-            res = np.array(res)
+        res = np.array(res)
         print("Timing:", np.mean(res), "+-", np.std(res))
         timings += [np.mean(res)]
         deviations += [np.std(res)]
@@ -39,3 +40,6 @@ if __name__ == "__main__":
         pickle.dump(timings, fp)
     with open("reports/distributed_datas/deviations", "wb") as fp:  # Pickling
         pickle.dump(deviations, fp)
+
+if __name__ == '__main__':
+    time_per_core()
