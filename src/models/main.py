a267888a077bc9cb8c962075f53a78bb78375c0f
Alessandro212977 <s212977@dtu.dk>
2022-01-20 10:55:44 +0100

    single image classification

--- src/models/main.py
+++ src/models/main.py
@@ -1,31 +1,44 @@
-from os import environ
+#from os import environ
 
 import torch
-import wandb
+#import wandb
 from conv_nn import ConvNet
-from omegaconf import OmegaConf
+#from omegaconf import OmegaConf
 # Imports the Google Cloud client library
 from google.cloud import storage
 import io
+from PIL import Image
+import torchvision.transforms.functional as TF
 
 
 def predict():
     print("Evaluating")
 
-    model_buffer, data_buffer = get_data('dtumlopsdata', 'models/trained_model.pt', 'data/processed/test.pt')
+    model_buffer, data_buffer = get_data('dtumlopsdata', 'models/trained_model.pt', 'data/raw/asl_alphabet_test/A_test.jpg')
 
     state_dict = torch.load(model_buffer)
     model = ConvNet(1024, 512)
     model.load_state_dict(state_dict)
 
-    test_data = torch.load(data_buffer)
-    test_set = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True)
 
-    config = OmegaConf.load("config.yaml")
+    image = Image.open(data_buffer)
+    x = TF.resize(image, 224)
+    x = TF.to_tensor(x)
+    x.unsqueeze_(0)
+    #print(x.shape)
+
+    ps = torch.exp(model(x))
+    top_p, top_class = ps.topk(1, dim=1)
+    return top_p.item(), top_class.item()
+
+    #test_data = torch.load(data_buffer)
+    #test_set = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True)
+
+    #config = OmegaConf.load("config.yaml")
     #environ["WANDB_API_KEY"] = config.wandb.api_key
     #environ["WANDB_MODE"] = config.wandb.mode
     #wandb.init(project=config.wandb.project, entity=config.wandb.entity)
-
+    """
     accuracies = []
 
     with torch.no_grad():
@@ -44,6 +57,7 @@ def predict():
 
     #wandb.log({"Test accuracy": accuracy.item()})
     print(f"Accuracy: {accuracy*100}%")
+    """
 
 
 def get_data(bucket, model_path, data_path):
@@ -51,10 +65,10 @@ def get_data(bucket, model_path, data_path):
     client = storage.Client()
 
     # Retrieve an existing bucket
-    bucket = client.get_bucket('dtumlopsdata')
+    bucket = client.get_bucket(bucket)
 
     # Then do other things...
-    model_blob = bucket.get_blob('models/trained_model.pt')
+    model_blob = bucket.get_blob(model_path)
     model_buffer = model_blob.download_as_string()
 
     # because model downloaded into string, need to convert it back
@@ -62,7 +76,7 @@ def get_data(bucket, model_path, data_path):
     print("model loaded")
 
     # Then do other things...
-    data_blob = bucket.get_blob('data/processed/test.pt')
+    data_blob = bucket.get_blob(data_path)
     data_buffer = data_blob.download_as_string()
 
     # because model downloaded into string, need to convert it back
